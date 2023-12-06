be1513ad210539de1b9e8d46efb555a3ae875ea1
mikeGugs <michaelrguglielmo@gmail.com>
2023-12-06 12:00:50 -0500

    Add a fabfile for automated deploys

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,51 @@
+import random
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+REPO_URL = 'https://github.com/mikeGugs/lists_app'
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
+
+def _get_latest_source():
+    if exists('.git'):
+        ## another custom modification to match my specific config
+        run('git checkout droplet')
+        run('git fetch')
+    else:
+        run(f'git clone -b droplet  {REPO_URL} .')
+        
+        ## Book instructions
+#    current_commit = local("git log -n 1 --format=%H", capture=True)
+#    run(f'git reset --hard {current_commit}')
+
+    ## Adjusting to match my config where the server uses the droplet branch
+    run('git merge origin/main')
+
+def _update_virtualenv():
+    if not exists('virtualenv/bin/pip'):
+        run(f'python3 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r requirements.txt')
+
+def _create_or_update_dotenv():
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+def _update_static_files():
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+
+def _update_database():
+    run('./virtualenv/bin/python manage.py migrate --noinput')
