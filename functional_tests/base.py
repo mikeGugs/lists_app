5e7638920d00ef13c21be3ef1add251086a03c53
mikeGugs <michaelrguglielmo@gmail.com>
2023-12-10 12:05:50 -0500

    rename all item input ids and names. still broken

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -39,3 +39,6 @@ class FunctionalTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(.5)
+
+    def get_item_input_box(self):
+        return self.browser.find_element('id', 'id_text')
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
@@ -11,7 +12,7 @@ def view_list(request, list_id):
     
     if request.method == 'POST':
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
@@ -22,7 +23,7 @@ def view_list(request, list_id):
     
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
