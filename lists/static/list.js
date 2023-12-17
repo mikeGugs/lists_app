4800bd66bdd903047db6995312a2f2d5dd37d3f9
mikeGugs <michaelrguglielmo@gmail.com>
2023-12-17 12:53:39 -0500

    add JS for hide error message on click of text box

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,7 +1,10 @@
 window.Superlists = {};
 window.Superlists.initialize = function () {
     $('input[name="text"]').on('keypress', function () {
-      $('.has-error').hide();
-    })
+	$('.has-error').hide();
+    });
+    $('input[name="text"]').on('click', function () {
+	$('.has-error').hide();
+    });
 };
 
