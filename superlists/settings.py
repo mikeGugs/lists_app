252c3a46e3cc17ac94eecee064f25571d9c03d4b
mikeGugs <michaelrguglielmo@gmail.com>
2023-12-31 12:05:50 -0500

    add logging to settings

--- superlists/settings.py
+++ superlists/settings.py
@@ -144,3 +144,20 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 LOGOUT_REDIRECT_URL = '/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
